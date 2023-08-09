959847a6214116f1af81219f319481a463227494
Tomi Malamud <tomasmalamud@gmail.com>
2023-08-09 12:14:26 -0300

    first adaptations

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Tomás Malamud',
+    site_url: 'https://tmalamud.com',
+    feed_url: 'https://tmalamud.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
